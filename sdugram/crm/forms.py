e983ad125022a953ea242968f5ca3644195b6e1f
Zhandos <200103398@stu.sdu.edu.kz>
2022-03-02 16:21:56 +0600

    Changed PIP

--- sdugram/crm/forms.py
+++ sdugram/crm/forms.py
@@ -3,9 +3,11 @@ from .models import Order
 
 
 class OrderForm(forms.Form):
-    class Meta:
-        model = Order
-        fields = ('title', 'image')
+
     name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
     phone = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
     img = forms.ImageField()
+
+    class Meta:
+        model = Order
+        fields = ('title', 'image')
--- sdugram/crm/views.py
+++ sdugram/crm/views.py
@@ -6,6 +6,7 @@ from price.models import PriceCard, PriceTable
 from telegrambot.sendMessage import sendTelegram
 from grid_panel.models import Advt
 
+
 # Create your views here.
 def first_page(request):
     slider_list = CmsSlider.objects.all()
@@ -17,36 +18,35 @@ def first_page(request):
     form = OrderForm()
 
     dict_obj = {'slider_list': slider_list,
-                 'pc_1': pc_1,
-                 'pc_2': pc_2,
-                 'pc_3': pc_3,
-                 'price_table': price_table,
-                 'form': form,
-                 'advt_list': advt_list }
+                'pc_1': pc_1,
+                'pc_2': pc_2,
+                'pc_3': pc_3,
+                'price_table': price_table,
+                'form': form,
+                'advt_list': advt_list}
     return render(request, './index.html', dict_obj)
+
 
 def Adboard(request):
     advt_list = Advt.objects.all()
     dict_obj = {'advt_list': advt_list}
     return render(request, './grid.html', dict_obj)
 
+
 def thanks_page(request):
     form = OrderForm(request.POST, request.FILES)
-    #img = request.POST['img']
+    # img = request.POST['img']
     if form.is_valid():
         form.save()
         # Get the current instance object to display in the template
     name = request.POST['name']
     phone = request.POST['phone']
 
-
-
-
-    #element = Order(order_name = name, order_phone = phone)
-    #element.save()
-    #img = 'order_img/' + img;
-    sendTelegram(tg_name = name, tg_phone = phone)
+    # element = Order(order_name = name, order_phone = phone)
+    # element.save()
+    # img = 'order_img/' + img;
+    sendTelegram(tg_name=name, tg_phone=phone)
 
     return render(request, './thanks.html', {'name': name,
-                                                  'phone': phone}
-                                                )
+                                             'phone': phone}
+                  )
