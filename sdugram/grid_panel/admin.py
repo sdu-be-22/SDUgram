1b1c7fe47a924ed0f9f92312f44c21c8a07d6325
jumagaliev1 <200103311@stu.sdu.edu.kz>
2022-04-03 15:07:22 +0600

    Added filters, added image for category

--- sdugram/grid_panel/admin.py
+++ sdugram/grid_panel/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Advt
+from .models import Advt, Category
 # Register your models here.
 admin.site.register(Advt)
+admin.site.register(Category)
