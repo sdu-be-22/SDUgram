5e57e53ad06cf5e7ac89fb321046526c149b9f6a
dinaberikkyzy1 <200103045@stu.sdu.edu.kz>
2022-05-22 18:04:05 +0600

    password and some changes

--- sdugram/sdugram/settings.py
+++ sdugram/sdugram/settings.py
@@ -145,11 +145,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = 'sdugram@gmail.com'
-EMAIL_HOST_PASSWORD = 'backendproject'
-EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'berikkyzydina03@gmail.com'
+EMAIL_HOST_PASSWORD = 'dikosya2003'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
